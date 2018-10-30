7014c2075afd5c9565f242873868893865178e34
Eduardo Braun <eduardo@rushers.org>
2018-10-29 22:49:48 -0300

    Fix package name

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 #![deny(clippy::all)]
-#[cfg_attr(
-    feature = "cargo-clippy",
-    allow(clippy::needless_pass_by_value)
-)]
-extern crate sharefem;
+#[cfg_attr(feature = "cargo-clippy", allow(clippy::needless_pass_by_value))]
+extern crate femapi;
 
 fn main() {
     sharefem::init_rocket().launch();
